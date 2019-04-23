2a02e59cd516b0649b1481f01479bc457f6a877e
Ramsey D'silva <rdsilva@mackenzieinvestments.com>
2019-04-23 10:04:29 -0400

    Push container to AWS ECR after build success

--- test_docker.sh
+++ test_docker.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-docker build -t $IMAGE_NAME .
+docker build -t $CONTAINER_NAME .
 echo Docker images:
 docker images
 docker run -d --name $CONTAINER_NAME $IMAGE_NAME
 echo Running docker containers:
 docker ps -a
 
-RUNNING=$(docker inspect -f {{.State.Running}} $CONTAINER_NAME)
+RUNNING=$(docker inspect -f {{.State.Running}} $IMAGE_NAME)
 
 if [ $RUNNING == true ]; then
 	exit 0
