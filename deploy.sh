34b04fc5736bbefefb81a1ffd71d9a6d1f7089b6
Jeff Li <jeff.li@mackenzieinvestments.com>
2023-05-13 07:51:48 -0400

    Deploy on master branch only

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+TAG=$(git tag | tail -n 1)
+
 docker tag "${IMAGE_NAME}:latest" "${REPOSITORY}/${IMAGE_NAME}:latest"
-docker tag "${IMAGE_NAME}:latest" "${REPOSITORY}/${IMAGE_NAME}:${TRAVIS_TAG}"
+docker tag "${IMAGE_NAME}:latest" "${REPOSITORY}/${IMAGE_NAME}:${TAG}"
 docker login --username "${REPOSITORY_USERNAME}" --password-stdin "${REPOSITORY}" < repository_password
 docker image push --all-tags "${REPOSITORY}/${IMAGE_NAME}"
